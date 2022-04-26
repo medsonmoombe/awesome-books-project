2498624770172b494b66963332f527957ebc9067
medsonmoombe <medsonmoombe21@gmail.com>
2022-04-26 12:48:56 +0200

    Add the class and function for the awesome book

--- script.js
+++ script.js
@@ -4,64 +4,69 @@ const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const addBtn = document.querySelector('.add-btn');
 
-let books = [];
-books = JSON.parse(localStorage.getItem('Awesome books')) !== null ? (books = JSON.parse(localStorage.getItem('books'))) : [];
 
-let listBooks;
-const getLocalData = () => {
-  listBooks = JSON.parse(localStorage.getItem('books'));
-};
+// refactor to use JavaScript classes
 
-// show books function
+class Library {
+  static books=[];
+   
+  listBooks;
 
-const showListBooks = () => {
-  bookList.innerHTML = '';
-  getLocalData();
-  listBooks.forEach((element) => {
-    const listTag = `
+  static addBook =(e) =>{
+    e.preventDefault();
+    this.books = JSON.parse(localStorage.getItem('books')) !== null ? (this.books = JSON.parse(localStorage.getItem('books'))) : [];
+
+      this.book = {
+          id: 0,
+          title: '',
+          author: '',
+      };
+
+      if (title.value === "" || author.value === "") {
+        return false;
+      }
+
+      this.book.title = title.value;
+      this.book.author = author.value;
+      this.book.id = this.books.length + 1;
+      this.books.push(this.book);
+      localStorage.setItem('books', JSON.stringify(this.books));
+      title.value = '';
+      author.value = '';
+  
+      
+      this.showListBooks();
+  }
+  static showListBooks = () => {
+    bookList.innerHTML = '';
+    this.listBooks = JSON.parse(localStorage.getItem('books'));
+    this.listBooks.forEach((element) => {
+      const listTag = `
             <div class="add-books">
               <p>"${element.title}" by ${element.author}</p>
               <button type="button" class="remove-btn" id="${element.id}">Remove</button>
             </div>
-          `;
-    bookList.innerHTML += listTag;
-  });
-};
-
-//   Addding books
-
-const addBook = () => {
-  books = JSON.parse(localStorage.getItem('books')) !== null ? (books = JSON.parse(localStorage.getItem('books'))) : [];
-  const book = {
-    id: 0,
-    title: '',
-    author: '',
-  };
-
-  book.title = title.value;
-  book.author = author.value;
-  book.id = books.length + 1;
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-  title.value = '';
-  author.value = '';
-
-  showListBooks();
-};
-
-addBtn.addEventListener('click', addBook);
-
-// Add addEventListener to the displaylist container
-
-bookList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('remove-btn')) {
-    const id = e.target.attributes.id.value;
-    const filteredBooks = listBooks.filter((book) => book.id !== +id);
-    localStorage.setItem(
-      'books',
-      JSON.stringify(filteredBooks),
-    );
-    showListBooks();
+        `;
+       bookList.innerHTML += listTag;
+    });
+    return this.bookList;
   }
-});
-document.addEventListener('DOMContentLoaded', showListBooks);
+
+       static deleteBooks =(e) => {
+        if (e.target.classList.contains('remove-btn')) {
+          const id = e.target.attributes.id.value;
+          const filteredBooks = this.listBooks.filter((book) => book.id !== +id);
+          this.listbooks = JSON.parse(localStorage.getItem('books'));
+          localStorage.setItem(
+            'books',
+            JSON.stringify(filteredBooks),
+          );
+          this.showListBooks();
+        }
+      }
+}
+
+
+addBtn.addEventListener('click', Library.addBook);
+bookList.addEventListener('click', Library.deleteBooks);
+document.addEventListener('DOMContentLoaded', Library.showListBooks);
