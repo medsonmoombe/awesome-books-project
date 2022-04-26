20b7b6346144c8857f612d1b4a307467eafa62bb
ilham bouaik <ibouaik@gmail.com>
2022-04-26 14:15:25 +0100

    fix linter errors

--- script.js
+++ script.js
@@ -4,39 +4,39 @@ const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const addBtn = document.querySelector('.add-btn');
 
-
 // refactor to use JavaScript classes
 
 class Library {
   static books=[];
-   
+
   listBooks;
 
-  static addBook =(e) =>{
+  static addBook =(e) => {
     e.preventDefault();
     this.books = JSON.parse(localStorage.getItem('books')) !== null ? (this.books = JSON.parse(localStorage.getItem('books'))) : [];
 
-      this.book = {
-          id: 0,
-          title: '',
-          author: '',
-      };
+    this.book = {
+      id: 0,
+      title: '',
+      author: '',
+    };
 
-      if (title.value === "" || author.value === "") {
-        return false;
-      }
+    if (title.value === '' || author.value === '') {
+      return false;
+    }
 
-      this.book.title = title.value;
-      this.book.author = author.value;
-      this.book.id = this.books.length + 1;
-      this.books.push(this.book);
-      localStorage.setItem('books', JSON.stringify(this.books));
-      title.value = '';
-      author.value = '';
-  
-      
-      this.showListBooks();
+    this.book.title = title.value;
+    this.book.author = author.value;
+    this.book.id = this.books.length + 1;
+    this.books.push(this.book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    title.value = '';
+    author.value = '';
+
+    this.showListBooks();
+    return true;
   }
+
   static showListBooks = () => {
     bookList.innerHTML = '';
     this.listBooks = JSON.parse(localStorage.getItem('books'));
@@ -47,25 +47,24 @@ class Library {
               <button type="button" class="remove-btn" id="${element.id}">Remove</button>
             </div>
         `;
-       bookList.innerHTML += listTag;
+      bookList.innerHTML += listTag;
     });
     return this.bookList;
   }
 
        static deleteBooks =(e) => {
-        if (e.target.classList.contains('remove-btn')) {
-          const id = e.target.attributes.id.value;
-          const filteredBooks = this.listBooks.filter((book) => book.id !== +id);
-          this.listbooks = JSON.parse(localStorage.getItem('books'));
-          localStorage.setItem(
-            'books',
-            JSON.stringify(filteredBooks),
-          );
-          this.showListBooks();
-        }
-      }
+         if (e.target.classList.contains('remove-btn')) {
+           const id = e.target.attributes.id.value;
+           const filteredBooks = this.listBooks.filter((book) => book.id !== +id);
+           this.listbooks = JSON.parse(localStorage.getItem('books'));
+           localStorage.setItem(
+             'books',
+             JSON.stringify(filteredBooks),
+           );
+           this.showListBooks();
+         }
+       }
 }
-
 
 addBtn.addEventListener('click', Library.addBook);
 bookList.addEventListener('click', Library.deleteBooks);
